69ce112a96acb33bfee379627ceeda59767391da
damienbod <damien_bod@hotmail.com>
2020-08-14 12:10:16 +0200

    init project

---
+++ ConsoleCreateEncyptedText/EncryptDecryptLib/EncryptText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EncryptDecryptLib
+{
+    public class EncryptText
+    {
+        private X509Certificate2 ReceiverPublicKeyCertificate;
+        private X509Certificate2 PrivateKeyCertificate;
+
+        public EncryptText()
+        {
+
+        }
+
+        public string Encrypt(string text)
+        {
+            return null;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Sending Encrypted Data
+
+
+# Links
+
+https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/configuration/overview
+
+https://edi.wang/post/2019/1/15/caveats-in-aspnet-core-data-protection
+
+https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.protecteddata.unprotect
+
+https://docs.microsoft.com/en-us/dotnet/standard/security/how-to-use-data-protection
