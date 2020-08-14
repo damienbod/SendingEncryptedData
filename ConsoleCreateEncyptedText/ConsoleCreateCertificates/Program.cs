5e254567f4c3aaf43a52678df8708238d44d0a6b
damienbod <damien_bod@hotmail.com>
2020-08-14 11:36:45 +0200

    Adding a certificate project

---
+++ ConsoleCreateEncyptedText/ConsoleCreateCertificates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleCreateCertificates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
