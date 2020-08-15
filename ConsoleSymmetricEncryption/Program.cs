6889333d87185620ebf08fd6361af32efc0a73dc
damienbod <damien_bod@hotmail.com>
2020-08-15 06:28:54 +0200

    remove text

--- ConsoleSymmetricEncryption/Program.cs
+++ ConsoleSymmetricEncryption/Program.cs
@@ -19,13 +19,13 @@ namespace ConsoleCreateEncryptedText
 
             var encryptedText = symmetricEncryptDecrypt.Encrypt(text, IVBase64, Key);
 
-            Console.WriteLine("-- Cipher Key --");
+            Console.WriteLine("-- Key --");
             Console.WriteLine(Key);
             Console.WriteLine("-- IVBase64 --");
             Console.WriteLine(IVBase64);
 
             Console.WriteLine("");
-            Console.WriteLine("-- Encrypted Text CipherTextBase64 --");
+            Console.WriteLine("-- Encrypted Text --");
             Console.WriteLine(encryptedText);
 
             var decryptedText = symmetricEncryptDecrypt.Decrypt(encryptedText, IVBase64, Key);
