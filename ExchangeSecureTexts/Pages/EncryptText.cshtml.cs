eef5f9b18d893540c676f0233477f62f35005600
damienbod <damien_bod@hotmail.com>
2020-08-15 15:30:49 +0200

    adding Encryption, Decryption helpers

--- ExchangeSecureTexts/Pages/EncryptText.cshtml.cs
+++ ExchangeSecureTexts/Pages/EncryptText.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EncryptDecryptLib;
+using ExchangeSecureTexts.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +11,19 @@ namespace ExchangeSecureTexts.Pages
 {
     public class EncryptTextModel : PageModel
     {
+        private readonly SymmetricEncryptDecrypt _symmetricEncryptDecrypt;
+        private readonly AsymmetricEncryptDecrypt _asymmetricEncryptDecrypt;
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public EncryptTextModel(SymmetricEncryptDecrypt symmetricEncryptDecrypt,
+            AsymmetricEncryptDecrypt asymmetricEncryptDecrypt,
+            ApplicationDbContext applicationDbContext)
+        {
+            _symmetricEncryptDecrypt = symmetricEncryptDecrypt;
+            _asymmetricEncryptDecrypt = asymmetricEncryptDecrypt;
+            _applicationDbContext = applicationDbContext;
+        }
+
         public void OnGet()
         {
         }
