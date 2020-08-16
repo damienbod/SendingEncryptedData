1ff95342cf0b60617e91b41869f87f9bff04c260
damienbod <damien_bod@hotmail.com>
2020-08-16 09:47:04 +0200

    Adding a decrypt page view

--- ExchangeSecureTexts/Pages/DecryptText.cshtml.cs
+++ ExchangeSecureTexts/Pages/DecryptText.cshtml.cs
@@ -1,7 +1,8 @@
-using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using CertificateManager;
 using EncryptDecryptLib;
 using ExchangeSecureTexts.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -14,18 +15,69 @@ namespace ExchangeSecureTexts.Pages
         private readonly SymmetricEncryptDecrypt _symmetricEncryptDecrypt;
         private readonly AsymmetricEncryptDecrypt _asymmetricEncryptDecrypt;
         private readonly ApplicationDbContext _applicationDbContext;
+        private readonly ImportExportCertificate _importExportCertificate;
+
+        [BindProperty]
+        public string Message { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string EncryptedMessage { get; set; }
 
         public DecryptTextModel(SymmetricEncryptDecrypt symmetricEncryptDecrypt,
             AsymmetricEncryptDecrypt asymmetricEncryptDecrypt,
-            ApplicationDbContext applicationDbContext)
+            ApplicationDbContext applicationDbContext,
+            ImportExportCertificate importExportCertificate)
         {
             _symmetricEncryptDecrypt = symmetricEncryptDecrypt;
             _asymmetricEncryptDecrypt = asymmetricEncryptDecrypt;
             _applicationDbContext = applicationDbContext;
+            _importExportCertificate = importExportCertificate;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                // Something failed. Redisplay the form.
+                return OnGet();
+            }
+
+            var cert = GetCertificateWithPrivateKeyForIdentity();
+
+            var encryptedDto = JsonSerializer.Deserialize<EncryptedDto>(EncryptedMessage);
+
+            var key = _asymmetricEncryptDecrypt.Encrypt(encryptedDto.Key,
+                _asymmetricEncryptDecrypt.CreateCipherPublicKey(cert));
+
+            var IV = _asymmetricEncryptDecrypt.Encrypt(encryptedDto.IV,
+              _asymmetricEncryptDecrypt.CreateCipherPublicKey(cert));
+
+            var text = _symmetricEncryptDecrypt.Decrypt(encryptedDto.EncryptedText, IV, key);
+
+            Message = $"{text}";
+
+            // Redisplay the form.
+            return OnGet();
+
+        }
+
+        private X509Certificate2 GetCertificateWithPrivateKeyForIdentity()
+        {
+            var user = _applicationDbContext.Users.First(user => user.Email == User.Identity.Name);
+
+            var certWithPublicKey = _importExportCertificate.PemImportCertificate(user.PemPublicKey);
+            var privateKey = _importExportCertificate.PemImportPrivateKey(user.PemPrivateKey);
+
+            var cert = _importExportCertificate.CreateCertificateWithPrivateKey(
+                certWithPublicKey, privateKey);
+
+            return cert;
         }
     }
 }
