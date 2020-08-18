4ac1c49da46b5446b8e691270cae606c7e0f791a
damienbod <damien_bod@hotmail.com>
2020-08-18 21:37:48 +0200

    improve naming

---
+++ EncryptDecryptLib/SignVerfiy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncryptDecryptLib
+{
+    public class SignVerfiy
+    {
+        // Sign with RSA using private key
+        public string Sign(string text, RSA rsa)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            byte[] signature = rsa.SignData(data, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            return Convert.ToBase64String(signature);
+        }
+
+        // Verify with RSA using public key
+        public bool Verify(string text, string signatureBase64, RSA rsa)
+        {
+            byte[] data = Convert.FromBase64String(text);
+            byte[] signature = Convert.FromBase64String(signatureBase64);
+            bool isValid = rsa.VerifyData(data, signature, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            return isValid;
+        }
+    }
+}
